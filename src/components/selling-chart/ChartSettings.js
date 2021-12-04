88bcce0776ea95c35123cdac5c3f105e2b44e7e4
unknown <soloducha.katarzyna@gmail.com>
2021-12-04 22:51:32 +0100

    added changing settings state

--- src/components/selling-chart/ChartSettings.js
+++ src/components/selling-chart/ChartSettings.js
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Stack from 'react-bootstrap/Stack'
-import Button from 'react-bootstrap/Button'
 import DropdowbButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 import './ChartSettings.css'
-import { timePeriods } from './Settings'
+import { chartTypes, measurements, showPreviousPeriodOptions, timePeriods } from './Settings'
 
 const ChartSettings = (props) => {
 
+    const [measurement, setMeasurement] = useState(props.initialMeasurement)
+    const [chartType, setChartType] = useState(props.initialChartType)
+    const [showPreviousPeriod, setShowPreviousPeriod] = useState(props.initialShowPreviousPeriod)
+    const [timePeriod, setTimePeriod] = useState(props.initialTimePeriod)
+
     const dropdownItems = timePeriods.map((timePeriod) => {
         return(
-            <Dropdown.Item>
+            <Dropdown.Item
+                eventKey={timePeriod}
+            >
                 {timePeriod}
             </Dropdown.Item>
         )
     })
+
+    const changeMeasurement = () => {
+        var newMeasurement = measurements.filter((m) => m !== measurement)
+        setMeasurement(newMeasurement)
+        props.onMeasurementChange(newMeasurement)
+    }
+
+    const changeChartType = () => {
+        var newChartType = chartTypes.filter((c) => c !== chartType)
+        setChartType(newChartType)
+        props.onChartTypeChange(newChartType)
+    }
+
+    const changeShowPreviousPeriod = () => {
+        var newShowPreviousPeriod = showPreviousPeriodOptions.filter((s) => s !== showPreviousPeriod)
+        setShowPreviousPeriod(newShowPreviousPeriod)
+        props.onShowPreviousPeriodChange(newShowPreviousPeriod)
+    }
+
+    const changeTimePeriod = (e) => {
+        setTimePeriod(e)
+        props.onTimePeriodChange(e)
+    }
 
     return(
         <Stack
@@ -31,7 +60,8 @@ const ChartSettings = (props) => {
                 >MEASUREMENT</label>
                 <label
                     className='clickable-text'
-                >{props.initialMeasurement}</label>
+                    onClick={changeMeasurement}
+                >{measurement}</label>
             </Stack>
             <Stack
                 direction='vertical'
@@ -42,7 +72,8 @@ const ChartSettings = (props) => {
                 >CHART TYPE</label>
                 <label
                     className='clickable-text'
-                >{props.initialChartType}</label>
+                    onClick={changeChartType}
+                >{chartType}</label>
             </Stack>
             <Stack
                 direction='vertical'
@@ -51,11 +82,11 @@ const ChartSettings = (props) => {
                 <label
                     className='label'
                 >SHOW PREVIOUS PERIOD</label>
-                <Button
-                    className='secondary-button previous-period-button'
-                >
-                    {props.initialShowPreviousPeriod}
-                </Button>
+                <label
+                    className='clickable-text'
+                    onClick={changeShowPreviousPeriod}
+                >{showPreviousPeriod}
+                </label>
             </Stack>
             <Stack
                 direction='vertical'
@@ -66,8 +97,9 @@ const ChartSettings = (props) => {
                 >TIME PERIOD</label>
                 <DropdowbButton
                     id='time-period-dropdown'
-                    title={props.initialTimePeriod}
+                    title={timePeriod}
                     variant='secondary'
+                    onSelect={changeTimePeriod}
                 >
                     {dropdownItems}
                 </DropdowbButton>
