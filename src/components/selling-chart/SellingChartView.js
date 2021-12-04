dd7efa00f76ac3910c4384395175494727e79c0b
unknown <soloducha.katarzyna@gmail.com>
2021-12-04 21:45:28 +0100

    added state handle methods to child

--- src/components/selling-chart/SellingChartView.js
+++ src/components/selling-chart/SellingChartView.js
@@ -6,21 +6,39 @@ import { chartTypes, measurements, timePeriods } from './Settings'
 
 const SellingChartView = () => {
 
-    const availableMeasurements = measurements
-    const availableChartTypes = chartTypes
-    const availableTimePeriods = timePeriods
-
-    const [measurement, setMeasurement] = useState(availableMeasurements[0])
-    const [chartType, setChartType] = useState(availableChartTypes[0])
+    const [measurement, setMeasurement] = useState(measurements[0])
+    const [chartType, setChartType] = useState(chartTypes[0])
     const [showPreviousPeriod, setShowPreviousPeriod] = useState(false)
-    const [timePeriod, setTimePerios] = useState(timePeriods[1])
+    const [timePeriod, setTimePeriod] = useState(timePeriods[1])
+
+    const handleMeasurementChange = (newMeasurement) => {
+        setMeasurement(this.newMeasurement)
+    }
+
+    const handleChartTypeChange = (newChartType) => {
+        setChartType(newChartType)
+    }
+
+    const handleShowPreviousPeriodChange = () => {
+        var newShowPreviousPeriod = !showPreviousPeriod
+        setMeasurement(this.newShowPreviousPeriod)
+    }
+
+    const handleTimePeriodChange = (newTimePeriod) => {
+        setTimePeriod(this.newTimePeriod)
+    }
 
     return(
         <div>
             <div
                 className='chart-settings'
             >
-                <ChartSettings></ChartSettings>
+                <ChartSettings 
+                    onMeasurementChange={handleMeasurementChange}
+                    onChartTypeChange={handleChartTypeChange}
+                    onShowPreviousPeriodChange={handleShowPreviousPeriodChange}
+                    onTimePeriodChange={handleTimePeriodChange}
+                />
             </div>
             <Button
                 className='secondary-button main-menu-button'
