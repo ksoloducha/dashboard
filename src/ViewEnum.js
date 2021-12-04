315507fa7d484e4b73de0eccd8f60918311b9f58
unknown <soloducha.katarzyna@gmail.com>
2021-12-04 11:20:42 +0100

    added exporting only widget views

--- src/ViewEnum.js
+++ src/ViewEnum.js
@@ -1,1 +1,3 @@
 export const views = ['Main window', 'Log in', 'Orders', 'Selling quality', 'Opinions', 'Offer ranking', 'Selling chart']
+
+export const widgetViews = ['Orders', 'Selling quality', 'Opinions', 'Offer ranking', 'Selling chart']
