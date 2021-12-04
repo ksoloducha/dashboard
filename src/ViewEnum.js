13e4391a6b31551b07d0454a7435f05ab3daf801
unknown <soloducha.katarzyna@gmail.com>
2021-12-04 10:57:02 +0100

    added view enum file

---
+++ src/ViewEnum.js
@@ -0,0 +1,13 @@
+export const views = {
+    MAINVIEW: 'Main view',
+    LOGIN: 'Log in',
+    ORDERS: 'Orders',
+    SELLINGQUALITY: 'Selling quality',
+    OPINIONS: 'Opinions',
+    OFFERRANKING: 'Offer ranking',
+    SELLINGCHART: 'Selling chart'
+}
+
+export function viewValues() {
+    return ['Orders', 'Selling quality', 'Opinions', 'Offer ranking', 'Selling chart']
+}
